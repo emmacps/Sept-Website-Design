85e6cdd51053b2bf00338ce380dc7ecd334cc9d0
emmacps <emmacps001@gmail.com>
2020-10-09 17:33:11 +0000

    starting javascript

---
+++ script.js
@@ -0,0 +1,16 @@
+//document.write("Say Cheers");
+
+/*function sayHello() {
+    alert("Hello Programmers");
+}*/
+
+var sayHello = "Coding";
+
+function sayHello() {
+    alert("Hello Programmers");
+}
+
+document.getElementById("mybtn").onclick = sayHello;
+
+
+
